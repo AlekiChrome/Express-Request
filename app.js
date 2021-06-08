fb6557a31cb682364a40b7360f18f31b5a652691
Candice Agard <candiceagard@candices-mbp.myfiosgateway.com>
2021-06-08 11:36:46 -0400

    Minerals route created

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // ! Dependencies
 const express = require("express");
+const rocks = require("./models/rock.js")
 
 // ! Configuration
 const app = express();
@@ -7,7 +8,20 @@ const app = express();
 // ! Routes
 app.get("/", (req, res) => {
     res.send("Welcome to the express request app")
-})
+});
+
+app.get("/rocks", (req, res) => {
+    res.send(rocks);
+});
+
+app.get("./rocks/:index", (req, res) => {
+    const index = req.params
+    if (rocks[index]) {
+        res.send(rocks[index])
+    } else {
+        res.send(`Sorry no rocks at [${index}] exists`);
+    }
+});
 
 // ! Export
 module.exports = app;
